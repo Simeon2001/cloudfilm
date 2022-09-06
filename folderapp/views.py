d45890121b2a026f5aa7642a45bd5ff6e62c5de9
Simeon2001 <jesusanyasimeon@gmail.com>
2022-09-06 14:27:20 +0100

    "folderapp view i won reverse later"

---
+++ folderapp/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, HttpResponse
+from box.models import Folder,UserStorageVolume
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+    authentication_classes,
+)
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from box.serializers import FolderSerial
+from box.hash import hashes
+
+# Create your views here.
+
+@api_view(["GET",'POST'])
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def index(request):
+    user = request.user
+    if request.method == "POST":
+        folder_name = request.data.get("name")
+        code = hashes()
+        visible = request.data.get("visible")
+        anyone_upload = request.data.get("anyone_upload")
+        data = Folder.objects.create(user=user, folder_name=folder_name, visible=visible,
+                                    code=code, anyone_upload=anyone_upload)
+        serializer_class = FolderSerial(data)
+        return Response(serializer_class.data, status=status.HTTP_201_CREATED)
+    else:
+        folder = Folder.objects.filter(user=user)
+        serializer_class = FolderSerial(folder,many=True)
+        return Response(serializer_class.data, status=status.HTTP_200_OK)
+
+
+@api_view(['DELETE', 'PUT'])
+@permission_classes([IsAuthenticated])
+def delete_folder(request, code):
+    if request.method == "DELETE":
+        current_user = request.user
+        try:
+            del_folder = Folder.objects.get(code=code)
+            if del_folder.user == current_user:
+                del_folder.delete()
+                return Response(status=200) 
+            else:
+                return Response(status=401)
+        except Folder.DoesNotExist:
+            return Response(status=404)
+    if request.method == "PUT":
+        try:
+            visible = request.data.get('visible')
+            return Response(status=200)
+            print(visible)
+        except:
+            return Response(status=404)
+            
+        try:
+            who_upload = request.data.get("anyone_upload")
+            return Response(status=200)
+            print(who_upload, "hello")
+        except:
+            return Response(status=404)
+            
+
+
+
+        
