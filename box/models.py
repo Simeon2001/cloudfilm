4eed5348d77621b23dd3f4da3bf59ac03636caef
Simeon2001 <jesusanyasimeon@gmail.com>
2022-09-20 03:11:27 +0100

    "intergrated google drive for storage"

--- box/models.py
+++ box/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from account.models import User
+from gdstorage.storage import GoogleDriveStorage
 
 # Create your models here.
 
+gd_storage = GoogleDriveStorage()
 
 class UserStorageVolume(models.Model):
     user = models.fieldName = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -28,6 +30,6 @@ class FileStorage(models.Model):
     idd = models.CharField(max_length=30, blank=False, default="vxc48sx7f")
     folder = models.ForeignKey(Folder, related_name="folders", on_delete=models.CASCADE)
     files_name = models.CharField(max_length=30000, blank=False, default="image.png")
-    files = models.ImageField(upload_to="store/%Y/%m/%d", blank=False)
+    files = models.ImageField(upload_to="store/%Y/%m/%d", blank=False, storage=gd_storage)
     image_description = models.TextField(max_length=10000, default="image")
     date_created = models.DateTimeField(auto_now_add=True)
