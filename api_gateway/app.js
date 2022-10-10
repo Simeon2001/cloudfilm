94af3d876e311679709fcbf8576a91bd0536fa90
Simeon2001 <jesusanyasimeon@gmail.com>
2022-10-10 17:41:38 +0100

    modularized my code and worked on image function auth and encode

--- api_gateway/app.js
+++ api_gateway/app.js
@@ -4,6 +4,8 @@ const app = express();
 const { Sequelize, DataTypes } = require('sequelize');
 require("dotenv").config({path: '../.env'})
 const sequelize = new Sequelize(process.env.POSTGRES_URL)
+const views = require('./views');
+
 try {
     sequelize.authenticate();
     console.log('connceted');
@@ -13,37 +15,23 @@ try {
 }
 
 const db = require("./models")(sequelize,DataTypes);
-
-db.sync();
+(async() => {
+    db.sync();
+})();
 
 app.use(express.json());
 
-
 hash = (key) => crypto.createHash('sha256').update(String(key)).digest('hex');
 
-app.get('/hello', (req,res) => {
-    b = req.headers.authorization,
-    res.send(hash(b)),
+app.get('/api/images', (req,res) => {
+    b = req.headers.authorization;
+    hashes = hash(b);
+    views.viewimage(req, res, hashes, db);
     console.log(b   , hash(b) );
 });
 
-app.post('/api/savekeys', (req,res) => {
-    if (req.headers.authorization == process.env.AUTH_TOKEN){
-
-        const enc_private_key  = req.body.pri_key;
-        const enc_public_key   = req.body.pub_key;
-        const token   = req.body.token;
-        if (enc_private_key.length == 0 | enc_public_key.length == 0){
-            return res.status(401).json({"status": false,})
-        }
-        const key_data = {enc_private: enc_private_key, enc_public: enc_public_key, token: token};
-        data = db.create(key_data)
-        console.log(data.token);
-        return res.status(200).json({"status": true,});
-    }
-    else {
-        return res.status(406).json({"status": false,"message": "not authenticated"});
-    }
+app.post('/api/savekeys', (req, res) => {
+    views.savekey(req, res, db);  
 });
 
 app.listen(9000, () => console.log("server ready"));
---
+++ api_gateway/views.js
@@ -0,0 +1,34 @@
+const savekey = async (req, res, db) => {
+    if (req.headers.authorization == process.env.AUTH_TOKEN){
+
+        const enc_private_key  = req.body.pri_key;
+        const enc_public_key   = req.body.pub_key;
+        const token   = req.body.token;
+        if (enc_private_key.length == 0 | enc_public_key.length == 0){
+            return res.status(401).json({"status": false,});
+        }
+        const key_data = {enc_private: enc_private_key, enc_public: enc_public_key, token: token};
+        
+        data = await db.create(key_data);
+        return res.status(200).json({"status": true,});
+        
+    }
+    else {
+        return res.status(406).json({"status": false,"message": "not authenticated"});
+    }
+}
+
+const viewimage = async (req,res, hashes, db) => {
+    const enc_privkey = await db.findOne({ 
+        where: { enc_private: hashes}});
+    if (enc_privkey === null){
+        return res.status(406).json({"status": false,"message": "invalid authentication token"});
+    } else{
+        const token = enc_privkey.token;
+        return res.status(200).json({"status": true, "message": token});
+    }
+}
+module.exports = {
+    savekey:savekey,
+    viewimage:viewimage
+}
